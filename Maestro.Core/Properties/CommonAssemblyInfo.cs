46d58bea22d581bec5323cdeb8e9001ea70fcf1c
Jonas Samuelsson <jonas@samuelsson.at>
2013-07-30 21:53:00 +0200

    Created initial project structure

---
+++ Maestro.Core/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Maestro.Core")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
