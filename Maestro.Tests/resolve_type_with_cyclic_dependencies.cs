dc1b1c2b9f8a089f5b3a6f346e0b42b033543e57
Jonas Samuelsson <jonas@samuelsson.at>
2013-08-11 21:07:17 +0200

    added test for cyclic dependencies

---
+++ Maestro.Tests/resolve_type_with_cyclic_dependencies.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Maestro.Tests
+{
+	public class resolve_type_with_cyclic_dependencies
+	{
+		[Fact]
+		public void should_throw_ActivationException()
+		{
+			Action act = () => new Container().Get<Foo>();
+			act.ShouldThrow<ActivationException>();
+		}
+
+		private class Foo { public Foo(Bar bar) { } }
+		private class Bar { public Bar(Foo foo) { } }
+	}
+}
