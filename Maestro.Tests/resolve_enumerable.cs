fc07fc281b6b049dbdc5726327ae4fb94de364f9
Jonas Samuelsson <jonas@samuelsson.at>
2013-08-11 23:31:09 +0200

    test 4 resolving named dependencies in GetAll

--- Maestro.Tests/resolve_enumerable.cs
+++ Maestro.Tests/resolve_enumerable.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System.Linq;
 using Xunit;
 
 namespace Maestro.Tests
@@ -11,6 +12,38 @@ namespace Maestro.Tests
 			var enumerable = new Container().GetAll<object>();
 
 			enumerable.Should().BeEmpty();
+		}
+
+		[Fact]
+		public void should_try_to_get_dependencies_with_same_name_as_top_instance()
+		{
+			var @defaultDependency = "default";
+			var namedDependency = "named";
+
+			var name = "yada yada";
+			var container = new Container(x =>
+			{
+				x.Add<Foo>().As<Foo>();
+				x.Add<Foo>(name).As<Foo>();
+
+				x.Default<object>().Is(@defaultDependency);
+				x.Add<object>(name).As(namedDependency);
+			});
+
+			var foos = container.GetAll<Foo>().ToList();
+
+			foos.Should().Contain(x => x.Object == defaultDependency);
+			foos.Should().Contain(x => x.Object == namedDependency);
+		}
+
+		private class Foo
+		{
+			public Foo(object o)
+			{
+				Object = o;
+			}
+
+			public object Object { get; private set; }
 		}
 	}
 }
