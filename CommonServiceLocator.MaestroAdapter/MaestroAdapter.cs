e3e79bef4e669e04ccdda90b7fbaf0727d74c065
Jonas Samuelsson <jonas@samuelsson.at>
2013-08-27 13:47:58 +0200

    CommonServiceLocator adapter

---
+++ CommonServiceLocator.MaestroAdapter/MaestroAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Maestro;
+using Microsoft.Practices.ServiceLocation;
+
+namespace CommonServiceLocator.MaestroAdapter
+{
+	public class MaestroAdapter : ServiceLocatorImplBase
+	{
+		private readonly IContainer _container;
+
+		public MaestroAdapter(IContainer container)
+		{
+			_container = container;
+		}
+
+		protected override object DoGetInstance(Type serviceType, string key)
+		{
+			return _container.Get(serviceType, key);
+		}
+
+		protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+		{
+			return _container.GetAll(serviceType);
+		}
+	}
+}
--- Maestro.Core/Properties/CommonAssemblyInfo.cs
+++ Maestro.Core/Properties/CommonAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Maestro.Core")]
+[assembly: AssemblyProduct("Maestro")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("Maestro.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100db433161a1ce90" +
-                                                       "7127aeb3136c24003dd1c643e291f771dd49407e206a37cf22197103619a0a7f8ba604f9bb623f" +
-                                                       "8dcd84d5c7f0cf5e5b1f6a5963e6e44640b2fa8af050dc99dec2b02df8e9db33710437166d2476" +
-                                                       "887ed703d1eff6daefb41bf5fe8d2d21df032e14d41b0da4815b9f045ac79222a84e138d19de08" +
-                                                       "dd8c93e6")]
+																		 "7127aeb3136c24003dd1c643e291f771dd49407e206a37cf22197103619a0a7f8ba604f9bb623f" +
+																		 "8dcd84d5c7f0cf5e5b1f6a5963e6e44640b2fa8af050dc99dec2b02df8e9db33710437166d2476" +
+																		 "887ed703d1eff6daefb41bf5fe8d2d21df032e14d41b0da4815b9f045ac79222a84e138d19de08" +
+																		 "dd8c93e6")]
