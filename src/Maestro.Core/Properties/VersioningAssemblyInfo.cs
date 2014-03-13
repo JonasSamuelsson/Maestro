1ced8e360ef2bcabefcff9a80d7b99a7bb2677a4
Jonas Samuelsson <jonas@samuelsson.at>
2014-03-13 22:35:44 +0100

    v1.4.0-rc

--- src/Maestro.Core/Properties/VersioningAssemblyInfo.cs
+++ src/Maestro.Core/Properties/VersioningAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")] // sync with major version in AssemblyFileVersion
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
