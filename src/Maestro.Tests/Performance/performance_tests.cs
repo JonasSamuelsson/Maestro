4030c706eb1c79fdf14b900deba3301fb3fcbcc2
Jonas Samuelsson <jonas@samuelsson.at>
2015-06-10 15:22:41 +0200

    cache factory in pipeline to improve performance

--- src/Maestro.Tests/Performance/performance_tests.cs
+++ src/Maestro.Tests/Performance/performance_tests.cs
@@ -9,34 +9,48 @@ namespace Maestro.Tests.Performance
 		const int Iterations = 10 * 1000;
 
 		[Fact]
-		public void reuse_container()
+		public void new_container()
 		{
-			var container = GetConfiguredContainer();
-			Console.WriteLine(Measure(() => container.Get<C3>()) / Measure(Baseline));
+			Warmup(Baseline);
+			Warmup(() => GetConfiguredContainer().Get<C3>());
+
+			Console.WriteLine(Measure(() => GetConfiguredContainer().Get<C3>()) / Measure(Baseline));
 		}
 
 		[Fact]
 		public void child_container()
 		{
+			Warmup(Baseline);
+			Warmup(() => GetConfiguredContainer().GetChildContainer().Get<C3>());
+
 			var container = GetConfiguredContainer();
 			Console.WriteLine(Measure(() => container.GetChildContainer().Get<C3>()) / Measure(Baseline));
 		}
 
 		[Fact]
-		public void new_container()
+		public void reuse_container()
 		{
-			Console.WriteLine(Measure(() => GetConfiguredContainer().Get<C3>()) / Measure(Baseline));
+			Warmup(Baseline);
+			Warmup(() => GetConfiguredContainer().Get<C3>());
+
+			var container = GetConfiguredContainer();
+			Console.WriteLine(Measure(() => container.Get<C3>()) / Measure(Baseline));
 		}
 
 		private static void Baseline()
 		{
-			new C3(new C0(), new C1(new C0()), new C2(new C0(), new C1(new C0())));
+			new C3(new C2(new C1(new C0()), new C0()), new C1(new C0()), new C0());
+		}
+
+		private static void Warmup(Action action)
+		{
+			action();
 		}
 
 		private static double Measure(Action action)
 		{
 			var stopwatch = Stopwatch.StartNew();
-			for (int i = 0; i < Iterations; i++) action();
+			for (var i = 0; i < Iterations; i++) action();
 			return stopwatch.Elapsed.TotalMilliseconds;
 		}
 
@@ -54,7 +68,7 @@ namespace Maestro.Tests.Performance
 
 		class C0 { }
 		class C1 { public C1(C0 c0) { } }
-		class C2 { public C2(C0 c0, C1 c1) { } }
-		class C3 { public C3(C0 c0, C1 c1, C2 c2) { } }
+		class C2 { public C2(C1 c1, C0 c0) { } }
+		class C3 { public C3(C2 c2, C1 c1, C0 c0) { } }
 	}
 }
