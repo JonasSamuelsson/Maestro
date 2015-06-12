0a8465d806e5a32deb5abe5c3decbbb6ab13b377
Jonas Samuelsson <jonas@samuelsson.at>
2015-06-12 14:22:01 +0200

    constructor invokation performance improvements

--- src/Maestro.Core/ConstructorInvokation.cs
+++ src/Maestro.Core/ConstructorInvokation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using Maestro.Internals;
 
@@ -15,14 +16,29 @@ namespace Maestro
 			Func<Context, object> func;
 			if (Cache.TryGetValue(constructor, out func)) return func;
 
-			if (dependencyProviders == null)
+			var parameters = constructor.GetParameters();
+			dependencyProviders = (dependencyProviders
+										  ?? parameters.Select(x => x.ParameterType)
+															.Select(x => new Func<Context, object>(ctx => ctx.Kernel.GetDependency(x, ctx)))).ToList();
+
+			if (dependencyProviders.Count() != parameters.Length)
 			{
-				var parameters = constructor.GetParameters();
-				dependencyProviders = parameters.Select(x => x.ParameterType)
-														  .Select(x => new Func<Context, object>(ctx => ctx.Kernel.GetDependency(x, ctx)));
+				throw new InvalidOperationException();
 			}
 
-			return ctx => constructor.Invoke(dependencyProviders.Select(x => x(ctx)).ToArray()); // todo perf
+			var parameterExpression = Expression.Parameter(typeof(Context), "ctx");
+			var index = 0;
+			var dependencyExpressions = new List<Expression>();
+			foreach (var dependencyProvider in dependencyProviders)
+			{
+				var type = parameters[index++].ParameterType;
+				Expression<Func<Context, object>> providerExpression = ctx => dependencyProvider(ctx);
+				var invokeExpression = Expression.Invoke(providerExpression, parameterExpression);
+				var castExpression = Expression.Convert(invokeExpression, type);
+				dependencyExpressions.Add(castExpression);
+			}
+			var newExpressions = Expression.New(constructor, dependencyExpressions);
+			return Cache[constructor] = Expression.Lambda<Func<Context, object>>(newExpressions, parameterExpression).Compile();
 		}
 	}
 }
