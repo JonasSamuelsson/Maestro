4705754239199996df8d26431878af8a380d90f0
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-04-28 23:05:13 +0200

    added ThreadSafeList

---
+++ src/Maestro.Core/Utils/ThreadSafeList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Maestro.Utils
+{
+	class ThreadSafeList<T> : IEnumerable<T>
+	{
+		private readonly object _root = new object();
+		private List<T> _list = new List<T>();
+
+		public void Add(T item)
+		{
+			lock (_root)
+			{
+				_list = new List<T>(_list) { item };
+			}
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return _list.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
