91786e2df416264dbaf4e320f418a9b916b07e33
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-04-28 16:47:59 +0200

    changed kernel -> parent relation

--- src/Maestro.Core/Internals/PluginLookup.cs
+++ src/Maestro.Core/Internals/PluginLookup.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Maestro.FactoryProviders;
-using Maestro.Interceptors;
 using Maestro.Lifetimes;
 
 namespace Maestro.Internals
