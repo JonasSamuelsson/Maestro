37c6d89ef42d21663a69fc57e3534fca7adc22f8
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-11-28 22:33:25 +0100

    removed dead code

--- src/Maestro.Core/Configuration/IContainerExpression.cs
+++ src/Maestro.Core/Configuration/IContainerExpression.cs
@@ -10,36 +10,6 @@ namespace Maestro.Configuration
 		IPluginExpression For(Type type, string name);
 		IPluginExpression<T> For<T>(string name);
 
-		///// <summary>
-		///// Used to configure instance, lifetime, interception etc for default instance of type <paramref name="type"/>.
-		///// </summary>
-		///// <param name="type"></param>
-		///// <returns></returns>
-		//IInstanceExpression<object> For(Type type);
-
-		///// <summary>
-		///// Used to configure instance, lifetime, interception etc for for default instance of type <typeparamref name="TPlugin"/>.
-		///// </summary>
-		///// <typeparam name="TPlugin"></typeparam>
-		///// <returns></returns>
-		//IInstanceExpression<TPlugin> For<TPlugin>();
-
-		///// <summary>
-		///// Used to configure instance, lifetime, interception etc for type <paramref name="type"/> named <paramref name="name"/>.
-		///// </summary>
-		///// <param name="type"></param>
-		///// <param name="name"></param>
-		///// <returns></returns>
-		//INamedInstanceExpression<object> For(Type type, string name);
-
-		///// <summary>
-		///// Used to configure instance, lifetime, interception etc for type <typeparamref name="TPlugin"/> named <paramref name="name"/>.
-		///// </summary>
-		///// <typeparam name="TPlugin"></typeparam>
-		///// <param name="name"></param>
-		///// <returns></returns>
-		//INamedInstanceExpression<TPlugin> For<TPlugin>(string name);
-
 		/// <summary>
 		/// Used for conventional configuration.
 		/// </summary>
