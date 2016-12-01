81de15e2fb2ba8881b409a60857d9a6fe2b4afa3
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-12-01 17:02:08 +0100

    added configuration compat shim

---
+++ src/Maestro.V1CompatibilityShim/Configuration/IDefaultPluginExpression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Maestro.Configuration
+{
+	public interface IDefaultPluginExpression : IPluginExpression
+	{
+		void Add(object instance);
+		IFactoryInstanceExpression<object> Add(Func<object> factory);
+		IFactoryInstanceExpression<object> Add(Func<IContext, object> factory);
+		ITypeInstanceExpression<object> Add(Type type);
+	}
+
+	public interface IDefaultPluginExpression<T> : IPluginExpression<T>
+	{
+		void Add(T instance);
+		IFactoryInstanceExpression<TInstance> Add<TInstance>(Func<TInstance> factory) where TInstance : T;
+		IFactoryInstanceExpression<TInstance> Add<TInstance>(Func<IContext, TInstance> factory) where TInstance : T;
+		ITypeInstanceExpression<TInstance> Add<TInstance>() where TInstance : T;
+	}
+}
