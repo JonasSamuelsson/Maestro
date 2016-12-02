e14d7a7a797637f793d40e1acfb007625183b351
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-12-02 14:48:57 +0100

    extracted IsEnumerable to Reflector

---
+++ src/Maestro.Tests/Utils/Reflector_tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Maestro.Utils;
+using Shouldly;
+using Xunit;
+
+namespace Maestro.Tests.Utils
+{
+	public class Reflector_tests
+	{
+		[Fact]
+		public void should_return_false_if_type_isnt_IEnumerable()
+		{
+			Reflector.IsEnumerable(typeof(string)).ShouldBe(false);
+			Reflector.IsEnumerable(typeof(IEnumerable)).ShouldBe(false);
+			Reflector.IsEnumerable(typeof(ICollection<object>)).ShouldBe(false);
+			Reflector.IsEnumerable(typeof(object[])).ShouldBe(false);
+			Reflector.IsEnumerable(typeof(IEnumerable<int>)).ShouldBe(false);
+			Reflector.IsEnumerable(typeof(IEnumerable<string>)).ShouldBe(false);
+		}
+
+		[Fact]
+		public void should_return_true_if_type_is_IEnumerable()
+		{
+			Reflector.IsEnumerable(typeof(IEnumerable<object>)).ShouldBe(true);
+			Reflector.IsEnumerable(typeof(IEnumerable<IDisposable>)).ShouldBe(true);
+		}
+	}
+}
