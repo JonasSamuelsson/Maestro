e4f38400646b3bbb0db279e51abf1db773dfcef1
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-12-07 16:54:36 +0100

    added Maestro.Microsoft.DependencyInjection project

---
+++ src/Maestro.Microsoft.DependencyInjection/Extensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Maestro.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Maestro.Microsoft.DependencyInjection
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Populates the container using the specified service descriptors.
+		/// </summary>
+		/// <remarks>
+		/// This method should only be called once per container.
+		/// </remarks>
+		/// <param name="container">The container.</param>
+		/// <param name="descriptors">The service descriptors.</param>
+		public static void Populate(this IContainer container, IEnumerable<ServiceDescriptor> descriptors)
+		{
+			AssertContainerIsntPopulatedTwice(container);
+
+			container.Configure(x =>
+			{
+				x.Service<IServiceProvider>().Use.Instance(new MaestroServiceProvider(container));
+
+				foreach (var descriptor in descriptors)
+				{
+					x.Register(descriptor);
+				}
+			});
+		}
+
+		private static void AssertContainerIsntPopulatedTwice(IContainer container)
+		{
+			Marker marker;
+			if (container.TryGetService(out marker))
+			{
+				throw new InvalidOperationException("Populate should only be called once per container.");
+			}
+
+			container.Configure(x => x.Service<Marker>().Use.Type<Marker>());
+		}
+
+		private static void Register(this IContainerExpression expression, ServiceDescriptor descriptor)
+		{
+			if (descriptor.ImplementationType != null)
+			{
+				expression.Service(descriptor.ServiceType)
+					 .Use.Type(descriptor.ImplementationType)
+					 .Lifetime.Use(descriptor.Lifetime);
+
+				return;
+			}
+
+			if (descriptor.ImplementationFactory != null)
+			{
+				expression.Service(descriptor.ServiceType)
+					 .Use.Factory(GetFactory(descriptor))
+					 .Lifetime.Use(descriptor.Lifetime);
+
+				return;
+			}
+
+			expression.Service(descriptor.ServiceType)
+				 .Use.Instance(descriptor.ImplementationInstance);
+		}
+
+		private static void Use<T>(this ILifetimeExpression<T> expression, ServiceLifetime descriptorLifetime)
+		{
+			switch (descriptorLifetime)
+			{
+				case ServiceLifetime.Singleton:
+					expression.Singleton();
+					break;
+				case ServiceLifetime.Scoped:
+					expression.Context();
+					break;
+				case ServiceLifetime.Transient:
+					expression.Transient();
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(descriptorLifetime), descriptorLifetime, null);
+			}
+		}
+
+		private static Func<IContext, object> GetFactory(ServiceDescriptor descriptor)
+		{
+			return context => descriptor.ImplementationFactory(context.GetService<IServiceProvider>());
+		}
+
+		private class Marker { }
+	}
+}
---
+++ src/Maestro.Microsoft.DependencyInjection/MaestroServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Maestro.Microsoft.DependencyInjection
+{
+	internal class MaestroServiceProvider : IServiceProvider
+	{
+		private readonly IContainer _container;
+
+		public MaestroServiceProvider(IContainer container)
+		{
+			_container = container;
+		}
+
+		public object GetService(Type serviceType)
+		{
+			object instance;
+			return _container.TryGetService(serviceType, out instance)
+				? instance
+				: null;
+		}
+	}
+}
