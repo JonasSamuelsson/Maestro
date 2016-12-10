de91c77a765b9546004bd75abc87d68895778223
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-12-10 23:18:57 +0100

    improved GetServices tests

--- src/Maestro.Tests/OldTests/services_tests.cs
+++ src/Maestro.Tests/OldTests/services_tests.cs
@@ -8,15 +8,25 @@ namespace Maestro.Tests.OldTests
 		[Fact]
 		public void should_get_services()
 		{
-			var @object = new object();
-
 			var container = new Container(x =>
 			{
-				x.Service<object>().Use.Type<object>();
-				x.Services<object>().Add.Instance(@object);
+				x.Services<object>().Add.Instance("success");
 			});
 
-			container.GetServices<object>().ShouldBe(new[] { @object });
+			container.GetServices<object>().ShouldBe(new[] { "success" });
+		}
+
+		[Fact]
+		public void should_not_include_default_or_named_instance()
+		{
+			var container = new Container(x =>
+			{
+				x.Service<object>().Use.Instance("fail");
+				x.Service<object>("foobar").Use.Instance("fail");
+				x.Services<object>().Add.Instance("success");
+			});
+
+			container.GetServices<object>().ShouldBe(new[] { "success" });
 		}
 	}
 }
