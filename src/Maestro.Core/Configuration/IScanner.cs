d2281a28909092fcde8077a4f973e3984b526f0a
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-12-16 22:56:38 +0100

    conventional configuration wip

---
+++ src/Maestro.Core/Configuration/IScanner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Maestro.Conventions;
+
+namespace Maestro.Configuration
+{
+	public interface IScanner
+	{
+		IScanner Assemblies(IEnumerable<Assembly> assemblies);
+		IScanner Assembly(Assembly assembly);
+		IScanner AssemblyContaining<T>();
+		IScanner AssemblyContaining(Type type);
+		IScanner AssemblyContainingTypeOf(object o);
+		IScanner Types(IEnumerable<Type> types);
+		IScanner Matching(Func<Type, bool> predicate);
+		IScanner Matching<T>() where T : IConventionFilter, new();
+		IScanner Matching(IConventionFilter filter);
+		IScanner With<T>() where T : IConvention, new();
+		IScanner With(IConvention convention);
+	}
+}
