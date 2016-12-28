3744c475f99cddd0961a27e49aaa33c25a758194
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-12-29 00:47:57 +0100

    refactoring

--- src/Maestro.Core/TypeFactoryResolvers/ConcreteClosedClassFactoryProviderResolver.cs
+++ src/Maestro.Core/TypeFactoryResolvers/ConcreteClosedClassFactoryProviderResolver.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Maestro.FactoryProviders;
 using Maestro.Internals;
+using Maestro.Utils;
 
 namespace Maestro.TypeFactoryResolvers
 {
@@ -11,18 +13,26 @@ namespace Maestro.TypeFactoryResolvers
 		public bool TryGet(Type type, Context context, out IFactoryProvider factoryProvider)
 		{
 			factoryProvider = null;
+
 			if (type == typeof(object)) return false;
 			if (!type.IsConcreteClosedClass()) return false;
 			if (type.IsArray) return false;
 			if (type.IsConcreteClassClosing(typeof(Func<>))) return false;
 			if (type.IsConcreteClassClosing(typeof(Lazy<>))) return false;
 
-			factoryProvider = (from ctor in type.GetConstructors(BindingFlags.Instance | BindingFlags.Public)
-									 let parameters = ctor.GetParameters()
-									 orderby parameters.Length descending
-									 where parameters.All(p => context.Kernel.CanGetService(p.ParameterType, context))
-									 select new TypeFactoryProvider(type) { Constructor = ctor }).FirstOrDefault();
+			factoryProvider = GetFactoryProviders(type, context).FirstOrDefault();
+
 			return factoryProvider != null;
+		}
+
+		private static IEnumerable<TypeFactoryProvider> GetFactoryProviders(Type type, Context context)
+		{
+			return from ctor in type.GetConstructors(BindingFlags.Instance | BindingFlags.Public)
+					 let parameters = ctor.GetParameters()
+					 orderby parameters.Length descending
+					 where parameters.All(p => !Reflector.IsPrimitiveGenericEnumerable(p.ParameterType))
+					 where parameters.All(p => context.Kernel.CanGetService(p.ParameterType, context))
+					 select new TypeFactoryProvider(type) { Constructor = ctor };
 		}
 	}
 }
