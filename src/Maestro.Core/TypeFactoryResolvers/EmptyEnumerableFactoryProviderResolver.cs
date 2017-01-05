b28c032a43c5f7dcffb200abb1d30a022c741f0e
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-01-06 00:46:27 +0100

    throwing NotSupportedException in EmptyEnumerableFactoryProviderResolver.MakeGeneric

--- src/Maestro.Core/TypeFactoryResolvers/EmptyEnumerableFactoryProviderResolver.cs
+++ src/Maestro.Core/TypeFactoryResolvers/EmptyEnumerableFactoryProviderResolver.cs
@@ -29,7 +29,7 @@ namespace Maestro.TypeFactoryResolvers
 
 			public IFactoryProvider MakeGeneric(Type[] genericArguments)
 			{
-				throw new NotImplementedException();
+				throw new NotSupportedException();
 			}
 		}
 
