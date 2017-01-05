ac5c1aca88353530a89fe1845d3a0906ac311ae4
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-01-05 13:22:08 +0100

    all tests passing =)

--- src/Maestro.Tests/OldTests/open_generic_type.cs
+++ src/Maestro.Tests/OldTests/open_generic_type.cs
@@ -64,37 +64,6 @@ namespace Maestro.Tests
 		}
 
 		[Fact]
-		public void get_all_should_handle_child_parent_container_configurations()
-		{
-			var container = new Container(x =>
-													{
-														x.For(typeof(Instance<>)).Use.Type(typeof(Instance<>)).SetProperty("Value", "parent-default");
-														x.For(typeof(Instance<>), "1").Use.Type(typeof(Instance<>)).SetProperty("Value", "parent-1");
-														x.For<Instance<string>>("2").Use.Type<Instance<string>>().SetProperty("Value", "parent-2");
-														x.For(typeof(Instance<>), "3").Use.Type(typeof(Instance<>)).SetProperty("Value", "parent-3");
-														x.For(typeof(Instance<>)).Add.Type(typeof(Instance<>)).SetProperty("Value", "parent");
-													})
-				.GetChildContainer(x =>
-										 {
-											 x.For(typeof(Instance<>)).Use.Type(typeof(Instance<>)).SetProperty("Value", "child-default");
-											 x.For(typeof(Instance<>), "1").Use.Type(typeof(Instance<>)).SetProperty("Value", "child-1");
-											 x.For(typeof(Instance<>), "2").Use.Type(typeof(Instance<>)).SetProperty("Value", "child-2");
-											 x.For<Instance<string>>("3").Use.Type<Instance<string>>().SetProperty("Value", "child-3");
-											 x.For(typeof(Instance<>)).Add.Type(typeof(Instance<>)).SetProperty("Value", "child");
-										 });
-
-			var instances = container.GetServices<Instance<string>>().ToList();
-
-			instances.Count.ShouldBe(6);
-			instances[0].Value.ShouldBe("child-3");
-			instances[1].Value.ShouldBe("child-default");
-			instances[2].Value.ShouldBe("child-1");
-			instances[3].Value.ShouldBe("child-2");
-			instances[4].Value.ShouldBe("child");
-			instances[5].Value.ShouldBe("parent");
-		}
-
-		[Fact]
 		public void GetServices_should_support_generic_type_definitions()
 		{
 			var container = new Container(x => x.For<object>().Add.Self());
