f92d7ff215ff5f9277f00d9e2cad12a7f8eb3dcb
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-01-06 01:20:36 +0100

    simplified LazyFactoryResolver_tests

--- src/Maestro.Tests/TypeFactoryResolvers/LazyFactoryResolver_tests.cs
+++ src/Maestro.Tests/TypeFactoryResolvers/LazyFactoryResolver_tests.cs
@@ -9,24 +9,17 @@ namespace Maestro.Tests.TypeFactoryResolvers
 		[Fact]
 		public void should_resolve_unregistered_lazy_of_resolvable_type()
 		{
-			var container = new Container();
-			var lazy = container.GetService<Lazy<ResolvableClass>>();
-			lazy.Value.ShouldNotBe(null);
+			var o = new object();
+			var container = new Container(x => x.For<object>().Use.Instance(o));
+			var lazy = container.GetService<Lazy<object>>();
+			lazy.Value.ShouldBe(o);
 		}
 
 		[Fact]
 		public void should_throw_trying_to_resolve_unregistered_lazy_of_unresolvable_type()
 		{
 			var container = new Container();
-			Should.Throw<ActivationException>(() => container.GetService<Lazy<UnresolvableClass>>());
-		}
-
-		class ResolvableClass { }
-
-		class UnresolvableClass
-		{
-			public UnresolvableClass(IDisposable disposable)
-			{ }
+			Should.Throw<ActivationException>(() => container.GetService<Lazy<object>>());
 		}
 	}
 }
