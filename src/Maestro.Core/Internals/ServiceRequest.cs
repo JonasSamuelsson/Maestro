3ce2e2d74927605a27bb894f4033952d0620e2a4
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-01-09 17:11:04 +0100

    expanded service request stack to include type & name

---
+++ src/Maestro.Core/Internals/ServiceRequest.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Maestro.Internals
+{
+	internal struct ServiceRequest
+	{
+		public ServiceRequest(Type type, string name)
+		{
+			Type = type;
+			Name = name;
+		}
+
+		public Type Type { get; }
+		public string Name { get; }
+
+		public bool Equals(ServiceRequest other)
+		{
+			return Type == other.Type && string.Equals(Name, other.Name);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			return obj is ServiceRequest && Equals((ServiceRequest)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((Type?.GetHashCode() ?? 0) * 397) ^ (Name?.GetHashCode() ?? 0);
+			}
+		}
+	}
+}
