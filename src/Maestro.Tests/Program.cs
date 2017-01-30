0e8754d3c6966d09c4eef119d75c9f43655a2ec9
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-01-30 09:57:59 +0100

    added test that lists public types

--- src/Maestro.Tests/Program.cs
+++ src/Maestro.Tests/Program.cs
@@ -1,12 +1,10 @@
-﻿using Maestro.Tests.Performance;
-
-namespace Maestro.Tests
+﻿namespace Maestro.Tests
 {
 	static class Program
 	{
 		static void Main()
 		{
-			new perf_tests().MultiThreaded();
+			new type_listings().list_types();
 		}
 	}
 }
