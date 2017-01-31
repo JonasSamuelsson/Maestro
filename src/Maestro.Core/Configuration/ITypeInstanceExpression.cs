8cfafc2a3bdab6dea1c1b0f1a207988a2d5b3898
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-01-31 13:48:03 +0100

    added support for custom constructor parameter values

--- src/Maestro.Core/Configuration/ITypeInstanceExpression.cs
+++ src/Maestro.Core/Configuration/ITypeInstanceExpression.cs
@@ -1,7 +1,20 @@
+using System;
+
 namespace Maestro.Configuration
 {
 	public interface ITypeInstanceExpression<T> : IInstanceExpression<T, ITypeInstanceExpression<T>>
 	{
 		ITypeInstanceExpression<T> CtorArg(string argName, object value);
+		ITypeInstanceExpression<T> CtorArg(string argName, Func<object> factory);
+		ITypeInstanceExpression<T> CtorArg(string argName, Func<IContext, object> factory);
+		ITypeInstanceExpression<T> CtorArg(string argName, Func<IContext, Type, object> factory);
+
+		ITypeInstanceExpression<T> CtorArg(Type argType, object value);
+		ITypeInstanceExpression<T> CtorArg(Type argType, Func<object> factory);
+		ITypeInstanceExpression<T> CtorArg(Type argType, Func<IContext, object> factory);
+
+		ITypeInstanceExpression<T> CtorArg<TValue>(TValue value);
+		ITypeInstanceExpression<T> CtorArg<TValue>(Func<TValue> factory);
+		ITypeInstanceExpression<T> CtorArg<TValue>(Func<IContext, TValue> factory);
 	}
 }
