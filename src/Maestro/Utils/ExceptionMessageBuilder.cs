0fa1ea0b1ad9d910ddbdcdbe64be49177c013b25
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-03-02 08:38:37 +0100

    improved error messages

---
+++ src/Maestro/Utils/ExceptionMessageBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Maestro.Utils
+{
+	internal static class ExceptionMessageBuilder
+	{
+		public static string GetMessage(string error, string reason)
+		{
+			return string.IsNullOrWhiteSpace(reason)
+				? error
+				: $"{error}{Environment.NewLine} > {reason}";
+		}
+	}
+}
