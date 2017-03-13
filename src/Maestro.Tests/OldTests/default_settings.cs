0a16e1ec072cbb6a02ca5c1306eeffd8679cea10
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-03-13 23:01:53 +0100

    added default lifetime tests

--- src/Maestro.Tests/OldTests/default_settings.cs
+++ src/Maestro.Tests/OldTests/default_settings.cs
@@ -23,6 +23,40 @@ namespace Maestro.Tests
 			convention.Types.ShouldBe(new[] { typeof(object) });
 		}
 
+		[Fact]
+		public void default_lifetime_should_be_transient()
+		{
+			var container = new Container(x => x.For<object>().Use.Self());
+
+			var instance1 = container.GetService<object>();
+			var instance2 = container.GetService<object>();
+
+			instance1.ShouldNotBe(instance2);
+		}
+
+		[Fact]
+		public void should_use_configured_default_lifetime()
+		{
+			var container = new Container(x =>
+			{
+				x.Default.Lifetime.Transient();
+				x.For<object>("transient").Use.Self();
+
+				x.Default.Lifetime.Singleton();
+				x.For<object>("singleton").Use.Self();
+			});
+
+			var transient1 = container.GetService<object>("transient");
+			var transient2 = container.GetService<object>("transient");
+
+			transient1.ShouldNotBe(transient2);
+
+			var singleton1 = container.GetService<object>("singleton");
+			var singleton2 = container.GetService<object>("singleton");
+
+			singleton1.ShouldBe(singleton2);
+		}
+
 		public class Convention : IConvention
 		{
 			public IEnumerable<Type> Types { get; private set; }
