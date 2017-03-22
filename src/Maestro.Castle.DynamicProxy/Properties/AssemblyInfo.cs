ae67d273becb303e552f37ea74bb20d59828bd2e
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-03-22 23:14:23 +0100

    converted mastro.castle.dynamicproxy to netstandard 1.3

--- src/Maestro.Castle.DynamicProxy/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Maestro.Interception.DynamicProxy")]
-[assembly: AssemblyVersion("1.0.0.0")] // sync with major version in AssemblyFileVersion
-[assembly: AssemblyFileVersion("1.0.2.0")]
