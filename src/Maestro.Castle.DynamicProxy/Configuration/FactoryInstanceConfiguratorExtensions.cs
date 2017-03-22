a3350c63ff1a5add7b9dff5b8c215fb575784095
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-03-22 23:46:02 +0100

    namespace & project structure in Maestro.Castle.DynamicProxy

--- src/Maestro.Castle.DynamicProxy/Configuration/FactoryInstanceConfiguratorExtensions.cs
+++ src/Maestro.Castle.DynamicProxy/Configuration/FactoryInstanceConfiguratorExtensions.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using Castle.DynamicProxy;
+using Maestro.Castle.DynamicProxy;
 
 namespace Maestro.Configuration
 {
-	public static class TypeInstanceExpressionExtensions
+	public static class FactoryInstanceConfiguratorExtensions
 	{
 		/// <summary>
 		/// Adds a pransparent proxy interceptor to the pipeline.
 		/// </summary>
 		/// <typeparam name="TInstance"></typeparam>
 		/// <typeparam name="TOut"></typeparam>
-		/// <param name="typeInstanceConfigurator"></param>
+		/// <param name="factoryInstanceConfigurator"></param>
 		/// <param name="proxyFactory"></param>
 		/// <returns></returns>
-		public static ITypeInstanceConfigurator<TOut> Proxy<TInstance, TOut>(this ITypeInstanceConfigurator<TInstance> typeInstanceConfigurator,
+		public static IFactoryInstanceConfigurator<TOut> Proxy<TInstance, TOut>(this IFactoryInstanceConfigurator<TInstance> factoryInstanceConfigurator,
 			Func<TInstance, ProxyGenerator, TOut> proxyFactory)
 		{
-			return typeInstanceConfigurator
+			return factoryInstanceConfigurator
 				.Intercept(new ProxyInterceptor<TInstance, TOut>((instance, context, proxyGenerator) => proxyFactory(instance, proxyGenerator)))
 				.As<TOut>();
 		}
@@ -26,13 +27,13 @@ namespace Maestro.Configuration
 		/// </summary>
 		/// <typeparam name="TInstance"></typeparam>
 		/// <typeparam name="TOut"></typeparam>
-		/// <param name="typeInstanceConfigurator"></param>
+		/// <param name="factoryInstanceConfigurator"></param>
 		/// <param name="proxyFactory"></param>
 		/// <returns></returns>
-		public static ITypeInstanceConfigurator<TOut> Proxy<TInstance, TOut>(this ITypeInstanceConfigurator<TInstance> typeInstanceConfigurator,
+		public static IFactoryInstanceConfigurator<TOut> Proxy<TInstance, TOut>(this IFactoryInstanceConfigurator<TInstance> factoryInstanceConfigurator,
 			Func<TInstance, IContext, ProxyGenerator, TOut> proxyFactory)
 		{
-			return typeInstanceConfigurator
+			return factoryInstanceConfigurator
 				.Intercept(new ProxyInterceptor<TInstance, TOut>(proxyFactory))
 				.As<TOut>();
 		}
