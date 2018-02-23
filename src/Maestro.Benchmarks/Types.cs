6820a0ea757f5d45661aece951ae51766dbdab41
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-02-23 07:42:04 +0100

    refactored benchmarks

---
+++ src/Maestro.Benchmarks/Types.cs
@@ -0,0 +1,23 @@
+﻿namespace Maestro.Benchmarks
+{
+	class Types
+	{
+		public class Transient { }
+
+		public class Scoped { }
+
+		public class Singleton { }
+
+		public class Generic<T> { }
+
+		public class WithProperty
+		{
+			public Transient Dependency { get; set; }
+		}
+
+		public class C0 { }
+		public class C1 { public C1(C0 c0) { } }
+		public class C2 { public C2(C1 c1, C0 c0) { } }
+		public class C3 { public C3(C2 c2, C1 c1, C0 c0) { } }
+	}
+}
