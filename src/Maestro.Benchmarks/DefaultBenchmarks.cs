d3722fe58da8621e06b18d5147c9007ec16c4631
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-02-23 13:24:35 +0100

    moved all types related to a default benchmark to that type/file

--- src/Maestro.Benchmarks/DefaultBenchmarks.cs
+++ src/Maestro.Benchmarks/DefaultBenchmarks.cs
@@ -3,8 +3,8 @@ using BenchmarkDotNet.Attributes.Jobs;
 
 namespace Maestro.Benchmarks
 {
-	[SimpleJob(launchCount: 1, warmupCount: 5, targetCount: 20, invocationCount: 50_000)]
-	public class Benchmarks
+	[MemoryDiagnoser, SimpleJob(launchCount: 1, warmupCount: 5, targetCount: 20, invocationCount: 10_000)]
+	public class DefaultBenchmarks
 	{
 		private static readonly IContainer Container = new Container(x =>
 		{
@@ -50,5 +50,26 @@ namespace Maestro.Benchmarks
 
 		[Benchmark]
 		public static void PropertyInjection() => Container.GetService<Types.WithProperty>();
+
+		class Types
+		{
+			public class Transient { }
+
+			public class Scoped { }
+
+			public class Singleton { }
+
+			public class Generic<T> { }
+
+			public class WithProperty
+			{
+				public Transient Dependency { get; set; }
+			}
+
+			public class C0 { }
+			public class C1 { public C1(C0 c0) { } }
+			public class C2 { public C2(C1 c1, C0 c0) { } }
+			public class C3 { public C3(C2 c2, C1 c1, C0 c0) { } }
+		}
 	}
 }
--- src/Maestro.Benchmarks/Program.cs
+++ src/Maestro.Benchmarks/Program.cs
@@ -1,64 +1,11 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using BenchmarkDotNet.Running;
-
-namespace Maestro.Benchmarks
+﻿namespace Maestro.Benchmarks
 {
 	class Program
 	{
-		public static void Main(string[] args) => BenchmarkRunner.Run<Benchmarks>();
+		//public static void Main() => BenchmarkRunner.Run<ChildContainerBenchmark>();
 
-		//public static void Main()
-		//{
-		//	var container = new Container(x => x.Use<object>().Self());
+		//public static void Main() => BenchmarkRunner.Run<DefaultBenchmarks>();
 
-		//	var stopwatch = Stopwatch.StartNew();
-		//	for (var i = 1; i <= 1_000_000; i++)
-		//	{
-		//		using (var childContainer = container.GetChildContainer())
-		//			childContainer.GetService<object>();
-
-		//		if (i % 100_000 == 0)
-		//		{
-		//			stopwatch.Stop();
-		//			var duration = stopwatch.Elapsed.TotalMilliseconds;
-		//			Console.WriteLine($"{i} {duration}ms");
-		//			PrintMemoryStats();
-		//			Console.WriteLine();
-		//			stopwatch.Start();
-		//		}
-		//	}
-		//}
-
-		//private static void PrintMemoryStats()
-		//{
-		//	var process = Process.GetCurrentProcess();
-
-		//	var properties = typeof(Process).GetProperties()
-		//		.Where(x => x.Name.Contains("64"));
-
-		//	foreach (var property in properties)
-		//		Console.WriteLine($"  {Format(property.Name)} {Format((long)property.GetValue(process, null))}");
-		//}
-
-		//private static string Format(string name)
-		//{
-		//	return name.Replace("64", "");
-		//}
-
-		//private static string Format(long value)
-		//{
-		//	var units = new[] { "b", "kB", "MB", "GB", "TB" };
-		//	var index = 0;
-		//	var d = (decimal)value;
-		//	while (d > 1024)
-		//	{
-		//		index++;
-		//		d /= 1024;
-		//	}
-
-		//	return $"{d:#.00}{units[index]}";
-		//}
+		public static void Main() => ManualBenchmark.Execute();
 	}
 }
