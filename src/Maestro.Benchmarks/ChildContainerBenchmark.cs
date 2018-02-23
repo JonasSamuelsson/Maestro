b3074d7334e4be88a89745cb19067f201e85e672
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-02-23 13:23:55 +0100

    added dedicated child container benchmark

---
+++ src/Maestro.Benchmarks/ChildContainerBenchmark.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Maestro.Benchmarks
+{
+	[MemoryDiagnoser]
+	public class ChildContainerBenchmark
+	{
+		static readonly Container Container = new Container(x => x.Use<object>().Self());
+
+		[Benchmark]
+		public void Execute()
+		{
+			using (var container = Container.GetChildContainer())
+				container.GetService<object>();
+		}
+	}
+}
