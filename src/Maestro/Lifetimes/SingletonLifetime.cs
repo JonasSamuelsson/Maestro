2e591be91d4e63f998132f86d217225172ede61e
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-06-14 21:34:16 +0200

    singleton lifetime performance

--- src/Maestro/Lifetimes/SingletonLifetime.cs
+++ src/Maestro/Lifetimes/SingletonLifetime.cs
@@ -1,23 +1,36 @@
-﻿using System;
+﻿using Maestro.Utils;
+using System;
 
 namespace Maestro.Lifetimes
 {
 	internal class SingletonLifetime : Lifetime
 	{
 		private readonly object _lock = new object();
+		private volatile object _instance;
 
 		public override object Execute(Context context, Func<Context, object> factory)
 		{
-			var scope = context.ScopedContainer.RootScope;
-			return scope.GetOrAdd(this, _ =>
+			if (_instance != null)
+				return _instance;
+
+			lock (_lock)
 			{
-				lock (_lock)
+				if (_instance != null)
+					return _instance;
+
+				var instance = factory.Invoke(context);
+
+				if (instance is IDisposable disposable)
 				{
-					return scope.TryGetValue(this, out var value)
-						? value
-						: factory.Invoke(context);
+					var proxy = new Disposable(() => disposable.Dispose());
+					if (!context.ScopedContainer.RootScope.TryAdd(this, proxy))
+						throw new InvalidOperationException();
 				}
-			});
+
+				_instance = instance;
+
+				return instance;
+			}
 		}
 
 		public override Lifetime MakeGeneric(Type[] genericArguments)
