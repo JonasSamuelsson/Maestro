0a71123ada619545716419b94ce35df8ef2cd6c3
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-06-14 21:34:16 +0200

    singleton lifetime performance

---
+++ src/Maestro/Utils/Disposable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Maestro.Utils
+{
+	public class Disposable : IDisposable
+	{
+		private readonly Action _action;
+
+		public Disposable(Action action)
+		{
+			_action = action;
+		}
+
+		public void Dispose()
+		{
+			_action.Invoke();
+		}
+	}
+}
