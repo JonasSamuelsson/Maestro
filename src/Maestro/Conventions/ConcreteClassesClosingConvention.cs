1354df5240fe3cdca4e9b3f80f33320c3c43840a
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-08-05 23:56:57 +0200

    updated conventional registration api

--- src/Maestro/Conventions/ConcreteClassesClosingConvention.cs
+++ src/Maestro/Conventions/ConcreteClassesClosingConvention.cs
@@ -7,9 +7,9 @@ namespace Maestro.Conventions
 	internal class ConcreteClassesClosingConvention : IConvention
 	{
 		private readonly Type _genericTypeDefinition;
-		private readonly Action<IConventionalServiceBuilder<object>> _action;
+		private readonly Action<IConventionalServiceRegistrator<object>> _action;
 
-		public ConcreteClassesClosingConvention(Type genericTypeDefinition, Action<IConventionalServiceBuilder<object>> action)
+		public ConcreteClassesClosingConvention(Type genericTypeDefinition, Action<IConventionalServiceRegistrator<object>> action)
 		{
 			_genericTypeDefinition = genericTypeDefinition;
 			_action = action;
@@ -20,7 +20,11 @@ namespace Maestro.Conventions
 			foreach (var type in types)
 			{
 				if (!type.IsConcreteClassClosing(_genericTypeDefinition, out var genericTypes)) continue;
-				genericTypes.ForEach(x => _action(new ConventionalServiceBuilder<object>(containerBuilder, x, type)));
+				genericTypes.ForEach(x =>
+				{
+					new ConventionalServiceBuilder<object>(containerBuilder, x, type)
+						.Execute(_action);
+				});
 			}
 		}
 	}
