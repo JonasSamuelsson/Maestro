e28dc902ad7d4b731275f68e308508ce7f5a99fe
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-08-06 21:57:46 +0200

    added ToServiceProvider methods on IScopedContainer & Context

---
+++ src/Maestro/ContextServiceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Maestro
+{
+	internal class ContextServiceProvider : IServiceProvider
+	{
+		private readonly Context _context;
+
+		public ContextServiceProvider(Context context)
+		{
+			_context = context;
+		}
+
+		public object GetService(Type serviceType)
+		{
+			// ReSharper disable once ConditionalTernaryEqualBranch
+			return _context.TryGetService(serviceType, out var instance) ? instance : instance;
+		}
+	}
+}
