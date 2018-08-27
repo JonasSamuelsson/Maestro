7d90fc5a664c9d4ffcf886bbc62c234c9e4742e8
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-08-27 09:49:29 +0200

    added disposed check to scope

--- test/Maestro.Tests/ContextServiceProviderTests.cs
+++ test/Maestro.Tests/ContextServiceProviderTests.cs
@@ -3,23 +3,23 @@ using Xunit;
 
 namespace Maestro.Tests
 {
-	public class ContextServiceProviderTests
-	{
-		[Fact]
-		public void ShouldGetService()
-		{
-			var container = new Container(x =>
-			{
-				x.Add<Root>().Factory(ctx => new Root { Dependency = ctx.ToServiceProvider().GetService(typeof(object)) });
-				x.Add<object>().Instance("success");
-			});
+   public class ContextServiceProviderTests
+   {
+      [Fact]
+      public void ShouldGetService()
+      {
+         var container = new Container(x =>
+         {
+            x.Add<Root>().Factory(ctx => new Root { Dependency = ctx.ToServiceProvider().GetService(typeof(object)) });
+            x.Add<object>().Instance("success");
+         });
 
-			container.GetService<Root>().Dependency.ShouldBe("success");
-		}
+         container.GetService<Root>().Dependency.ShouldBe("success");
+      }
 
-		class Root
-		{
-			public object Dependency { get; set; }
-		}
-	}
+      private class Root
+      {
+         public object Dependency { get; set; }
+      }
+   }
 }
