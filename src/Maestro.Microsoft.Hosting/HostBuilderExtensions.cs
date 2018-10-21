f9453571576c7c7b8df346e3ec43553c6f009d27
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-10-21 16:54:48 +0200

    Maestro.Microsoft.Hosting v1.0.0

---
+++ src/Maestro.Microsoft.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Maestro.Configuration;
+using Maestro.Microsoft.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Maestro.Microsoft.Hosting
+{
+	public static class HostBuilderExtensions
+	{
+		public static IHostBuilder ConfigureContainer(this IHostBuilder hostBuilder, Action<ContainerBuilder> configureDelegate)
+		{
+			return hostBuilder.ConfigureContainer((context, containerBuilder) => configureDelegate.Invoke(containerBuilder));
+		}
+
+		public static IHostBuilder ConfigureContainer(this IHostBuilder hostBuilder, Action<HostBuilderContext, ContainerBuilder> configureDelegate)
+		{
+			return hostBuilder.ConfigureContainer<ContainerBuilder>(configureDelegate.Invoke);
+		}
+
+		public static IHostBuilder UseMaestro(this IHostBuilder hostBuilder)
+		{
+			return hostBuilder.UseServiceProviderFactory(new MaestroServiceProviderFactory());
+		}
+	}
+}
